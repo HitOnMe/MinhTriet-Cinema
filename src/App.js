2836760817eb7f7cd458011d963ec1fb6b07e946
MTriet <minhtrietkhuu3@gmail.com>
2024-11-05 10:10:59 +0700

    checking branch from Triet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import TicketRoom from './Template/Cinema/Cinema';
-import Admin from './Template/AdminTemplate/Admin';
-import HomeTemplate from './Template/HomeTemplate/HomeTemplate';
-import MoveSlider from './Template/Ticket/MoveSlider';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import TicketRoom from "./Template/Cinema/Cinema";
+import Admin from "./Template/AdminTemplate/Admin";
+import HomeTemplate from "./Template/HomeTemplate/HomeTemplate";
+import MoveSlider from "./Template/Ticket/MoveSlider";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path='/admin' element={<Admin />} />
-        <Route path='/' element={<HomeTemplate />} />
-        <Route path='/cinema' element={<TicketRoom />} />
-        <Route path='/ticket' element={<MoveSlider />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<HomeTemplate />} />
+        <Route path="/cinema" element={<TicketRoom />} />
+        <Route path="/ticket" element={<MoveSlider />} />
       </Routes>
     </HashRouter>
   );
