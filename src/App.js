2a99379c5bbfb62293a74f547fa977268bc3a1ed
HitOnMe <levietminh93@gmail.com>
2024-11-05 09:22:55 +0700

    fix route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-
-import './App.css';
-import {Router} from 'react-router'
-import { createBrowserHistory } from 'history';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import TicketRoom from './Template/Cinema/Cinema';
 import Admin from './Template/AdminTemplate/Admin';
 import HomeTemplate from './Template/HomeTemplate/HomeTemplate';
-import MoveSlider from './Template/Ticket/MoveSlider'
-export const history = createBrowserHistory();
+import MoveSlider from './Template/Ticket/MoveSlider';
 
 function App() {
   return (
-    <BrowserRouter basename="/MinhTriet-Cinema">
-    <Routes>
-      <Route path='/admin' element={<Admin />} />
-      <Route path='/' element={<HomeTemplate />} />
-      <Route path='/cinema' element={<TicketRoom />} />
-      <Route path="/ticket" element={<MoveSlider />} />
-    </Routes>
-  </BrowserRouter>
-
+    <HashRouter>
+      <Routes>
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/' element={<HomeTemplate />} />
+        <Route path='/cinema' element={<TicketRoom />} />
+        <Route path='/ticket' element={<MoveSlider />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
