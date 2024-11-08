2f5ce2e390218daa18844b078bfb8e76e676c329
HitOnMe <levietminh93@gmail.com>
2024-11-08 18:42:25 +0700

    change

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ export const history = createBrowserHistory();
 
 function App() {
   return (
-
     <div>
       <Spinner />
       <BrowserRouter>
@@ -25,18 +24,19 @@ function App() {
           <Route path="/" element={<HomeTemplate content={<HomePage />} />} />
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/register" element={<RegisterPage />}></Route>
-          {/* Account gặp vấn đề về render khi đổ dữ liệu */}
           {/* <Route path="/account" element={<AccountPage />}></Route> */}
           <Route
             path="/detail/:id"
             element={<HomeTemplate content={<DetailPage />} />}
           ></Route>
           <Route path="/ticket" element={<TicketRoom />} />
-          <Route
-            path="/MinhTriet-Cinema/Template/Ticket/ticket"
-            element={<Ticket />}
-          />
-          <Route path="*" element={<Navigate to="/" />}></Route>
+          <Route path="/MinhTriet-Cinema/Template/Ticket/ticket" element={<Ticket />} />
+          
+          {/* Các route của nhánh Minh */}
+          <Route path='/admin' element={<Admin />} />
+          <Route path='/cinema' element={<TicketRoom />} />
+          <Route path="/ticket" element={<TicketBookingLayout />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
