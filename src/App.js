392df7e02e478aeeb7cc831f7482c41400bd63d3
HitOnMe <levietminh93@gmail.com>
2024-11-08 18:35:07 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import { createBrowserHistory } from "history";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -11,21 +10,13 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import DetailPage from "./pages/DetailPage/DetailPage";
 import AccountPage from "./pages/AccountPage/AccountPage";
-=======
+import Admin from "./Template/AdminTemplate/Admin";
+import TicketBookingLayout from "./Template/Ticket/ticket";
 
-import './App.css';
-import { createBrowserHistory } from 'history';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import TicketRoom from './Template/Cinema/Cinema';
-import Admin from './Template/AdminTemplate/Admin';
-import HomeTemplate from './Template/HomeTemplate/HomeTemplate';
-import TicketBookingLayout from './Template/Ticket/ticket';
->>>>>>> Minh
 export const history = createBrowserHistory();
 
 function App() {
   return (
-<<<<<<< HEAD
     <div>
       <Spinner />
       <BrowserRouter>
@@ -33,32 +24,22 @@ function App() {
           <Route path="/" element={<HomeTemplate content={<HomePage />} />} />
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/register" element={<RegisterPage />}></Route>
-          {/* Account gặp vấn đề về render khi đổ dữ liệu */}
           {/* <Route path="/account" element={<AccountPage />}></Route> */}
           <Route
             path="/detail/:id"
             element={<HomeTemplate content={<DetailPage />} />}
           ></Route>
           <Route path="/ticket" element={<TicketRoom />} />
-          <Route
-            path="/MinhTriet-Cinema/Template/Ticket/ticket"
-            element={<Ticket />}
-          />
-          <Route path="*" element={<Navigate to="/" />}></Route>
+          <Route path="/MinhTriet-Cinema/Template/Ticket/ticket" element={<Ticket />} />
+          
+          {/* Các route của nhánh Minh */}
+          <Route path='/admin' element={<Admin />} />
+          <Route path='/cinema' element={<TicketRoom />} />
+          <Route path="/ticket" element={<TicketBookingLayout />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
-=======
-    <BrowserRouter>
-    <Routes>
-      <Route path='/admin' element={<Admin />} />
-      <Route path='/' element={<HomeTemplate />} />
-      <Route path='/cinema' element={<TicketRoom />} />
-      <Route path="/ticket" element={<TicketBookingLayout />} />
-    </Routes>
-  </BrowserRouter>
-
->>>>>>> Minh
   );
 }
 
